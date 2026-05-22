895eb85855c173b84945c60b44f57439b29685ad
jfelipurDAW <jfelipur21@alumnes.ilerna.com>
2026-05-22 19:35:01 +0200

    Update PasswordValidatorTest.java

--- PasswordValidatorTest.java
+++ PasswordValidatorTest.java
@@ -1,24 +1,34 @@
-@Test
-void nullPasswordShouldBeInvalid() {
-    assertFalse(PasswordValidator.isValid());
-}
- 
- 
-@Test
-void shortPasswordShouldBeInvalid() {
-    assertFalse(PasswordValidator.isValid("abc"));
+package junit_test;
 
-}
- 
- 
-@Test
-void passwordWithoutNumbersShouldBeInvalid() {
-    assertFalse(PasswordValidator.isValid("Contrassenya"));
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
+public class PasswordValidatorTest {
+
+	@Test
+	void nullPasswordShouldBeInvalid() {
+		PasswordValidator pv = new PasswordValidator();
+		assertFalse(pv.isValid(null));
+	}
+
+	@Test
+	void shortPasswordShouldBeInvalid() {
+		PasswordValidator pv = new PasswordValidator();
+		assertFalse(pv.isValid("abc"));
+
+	}
+
+	@Test
+	void passwordWithoutNumbersShouldBeInvalid() {
+		PasswordValidator pv = new PasswordValidator();
+		assertFalse(pv.isValid("Contrassenya"));
+
+	}
+
+	@Test
+	void validPasswordShouldReturnTrue() {
+		PasswordValidator pv = new PasswordValidator();
+		assertTrue(pv.isValid("Passw0rd"));
+	}
 }
- 
- 
-@Test
-void validPasswordShouldReturnTrue() {
-    assertTrue(PasswordValidator.isValid("Passw0rd"));
-} 
